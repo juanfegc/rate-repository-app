5dee76d035f42c1dfc92f863f90d2543bd90e1f9
juanfer <juanfer@pop-os.localdomain>
2022-03-16 19:10:51 +0100

    styles 10.5

--- src/components/RepositoryListItem.jsx
+++ src/components/RepositoryListItem.jsx
@@ -6,31 +6,74 @@ import theme from '../theme'
 const millar = num => {
   return num + 'k'
 }
+
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
-    flexGrow: 1,
     flexDirection: 'column',
-    alignItems: 'flex-end',
+  },
+  containerHeader: {
+    flexDirection: 'row',
+  },
+  avatarContainer: {
+    flexGrow: 0,
+    padding: 20,
+    paddingTop: 4,
+  },
+  infoContainer: {
+    flexGrow: 1,
+    flexWrap: 'wrap',
   },
   avatar: {
-    width: 50,
-    height: 50,
+    width: 55,
+    height: 55,
+    borderRadius: 8,
   },
   boton: {
     backgroundColor: theme.colors.primary,
+    alignSelf: 'stretch',
   },
+  containerFooter: {
+    flexDirection: 'row',
+    flexGrow: 1,
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  containerFooterItem: { flexDirection: 'column' },
+  containerFooterNumber: { fontWeight: 'bold' },
 })
+
 const RepositoryItem = ({ item }) => (
   <View style={styles.container}>
-    <Image style={styles.avatar} source={{ uri: item.ownerAvatarUrl }} />
-    <Text>Fullname: {item.fullName}</Text>
-    <Text>Description: {item.description}</Text>
-    <Text style={styles.boton}>Language: {item.language}</Text>
-    <Text>Stars: {item.stargazersCount}</Text>
-    <Text>Forks: {item.forksCount}</Text>
-    <Text>Reviews: {item.reviewCount}</Text>
-    <Text>Rating: {item.ratingAverage}</Text>
+    <View style={styles.containerHeader}>
+      <View style={styles.avatarContainer}>
+        <Image style={styles.avatar} source={{ uri: item.ownerAvatarUrl }} />
+      </View>
+      <View style={styles.infoContainer}>
+        <Text>{item.fullName}</Text>
+        <Text>{item.description}</Text>
+        <Text style={styles.boton}>{item.language}</Text>
+      </View>
+    </View>
+
+    <View style={styles.containerFooter}>
+      <View style={styles.containerFooterItem}>
+        <Text style={styles.containerFooterNumber}>{item.stargazersCount}</Text>
+        <Text>Stars</Text>
+      </View>
+      <View style={styles.containerFooterItem}>
+        <Text style={styles.containerFooterNumber}>{item.forksCount}</Text>
+        <Text>Forks</Text>
+      </View>
+      <View style={styles.containerFooterItem}>
+        <Text style={styles.containerFooterNumber}>{item.reviewCount}</Text>
+        <Text>Reviews</Text>
+      </View>
+      <View style={styles.containerFooterItem}>
+        <Text style={styles.containerFooterNumber}>{item.ratingAverage}</Text>
+        <Text>Rating</Text>
+      </View>
+    </View>
   </View>
 )
 
